5b835e1b2e08a0f0ce3a95f377f4e13bcd7a41b4
litelte <litecaoxzero98@foxmail.com>
2022-05-13 12:51:53 +0800

    test

---
+++ chapter9/day18/day18v1.py
@@ -0,0 +1,10 @@
+from random import randint
+
+randint(1, 6)
+from random import choice
+
+player = ['charles', 'martina', 'michael', 'florence', 'eli']
+first_up = choice(player)
+first_up
+print(first_up)
+
